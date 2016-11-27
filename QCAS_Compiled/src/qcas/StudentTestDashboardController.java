729d2d13396dca74abe9627fc074a98202fed37c
paridhichoudhary <paridhi.chdhry@gmail.com>
2016-11-27 15:19:01 +1030

    Addding Files

--- QCAS_Compiled/src/qcas/StudentTestDashboardController.java
+++ QCAS_Compiled/src/qcas/StudentTestDashboardController.java
@@ -9,12 +9,17 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -24,36 +29,71 @@ import javafx.stage.Stage;
  * @author Meeth
  */
 public class StudentTestDashboardController implements Initializable {
-@FXML
-private Button startTestButton;
-@FXML
-String difficultyLevel;
-@FXML
-int numberOfQuestions;
-@FXML
+    @FXML
+    private Button startTestButton;
+    @FXML
+    String difficultyLevel;
+    @FXML
+    private ComboBox studentTComboBox;
+    @FXML
+    int numberOfQuestions;
+    @FXML 
+    private RadioButton RB1;
+    @FXML 
+    private RadioButton RB2;
+    @FXML 
+    private RadioButton RB3;
+    @FXML
+    private ToggleGroup studentTG;
+    @FXML
     TestController t;
-public StudentTestDashboardController() throws IOException, SQLException{
-    
-}
+    @FXML
+    private ObservableList comboData = FXCollections.observableArrayList();
+        
+        
+    public StudentTestDashboardController() throws IOException, SQLException{
+
+    }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+       comboData.add(15);
+       comboData.add(20);
+       comboData.add(25);
+       studentTComboBox.setItems(comboData);
+       
+       RB1.setToggleGroup(studentTG);
+       RB2.setToggleGroup(studentTG);
+       RB3.setToggleGroup(studentTG);
+       
+
+       
     }    
     @FXML
-    private void goToTest(MouseEvent event) throws IOException {
-        FXMLLoader n = new FXMLLoader().load(getClass().getResource("Test.fxml"));
-        TestController testController = (n.getController());
-        testController.setDifficultyLevel(this.difficultyLevel);
-        testController.setNumberOfQuestions(this.numberOfQuestions);
-        //Parent root = FXMLLoader.load(getClass().getResource("Test.fxml"));
+    private void goToTest(MouseEvent event) throws IOException, SQLException {       
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("Test.fxml"));
+        numberOfQuestions= (int)studentTComboBox.getSelectionModel().getSelectedItem();
+        if(RB1.isSelected()){
+           difficultyLevel= "E";
+       }
+       if(RB2.isSelected()){
+           difficultyLevel= "M";
+       }
+       if(RB3.isSelected()){
+           difficultyLevel= "H";
+       }
+        Parent root = (Parent) loader.load();
+        TestController testController = loader.<TestController> getController().initialize(difficultyLevel, numberOfQuestions);
         Stage stage = (Stage) startTestButton.getScene().getWindow();
-        Scene scene = new Scene(n.getRoot());
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.getOwner();
         stage.show();
+        
     }
     
 }
